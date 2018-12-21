0aee60054f99d1e398d4face6386f41fbfbd4634
Cameron Miller <lunarrocketeer@gmail.com>
2018-12-20 22:57:04 -0800

    Initial commit.

---
+++ EmpireSim/Asset.py
@@ -0,0 +1,4 @@
+class Asset(object):
+    """description of class"""
+
+
---
+++ EmpireSim/Empire.py
@@ -0,0 +1,13 @@
+class Empire(object):
+    """description of class"""
+
+ 
+    def __init__(self):
+        self.name = "name"
+        self.assets = []
+        self.affinity = []
+        self.hp = 0
+        self.strength = 0
+        self.cunning = 0
+        self.credits = 0
+
